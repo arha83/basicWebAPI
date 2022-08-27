5e2d44ac954a133030fb0ae4ff0f8878bb0916e8
arha83 <arha113918@gmail.com>
2022-08-27 11:08:44 +0430

    removed urls with no slash at end deleted unnecessary code in settings

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('api', views.apiRoot),
+    #path('api', views.apiRoot),
     path('api/', views.apiRoot),
-    path('api/vars', views.varsList),
+    #path('api/vars', views.varsList),
     path('api/vars/', views.varsList),
-    path('api/vars/<int:pk>', views.varsDetail),
+    #path('api/vars/<int:pk>', views.varsDetail),
     path('api/vars/<int:pk>/', views.varsDetail),
 ]
